8cfd091c11ab55c31c980191f35469f604f52fa3
shivarajshanthaiah <shivarajshanthaiah@gmai.com>
2024-05-28 10:32:41 +0530

    stackToQueue vise versa

---
+++ QueueToStack/WithLinkedList/main.go
@@ -0,0 +1,109 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type Stack struct {
+	head *Node
+}
+
+func (s *Stack) push(value int) {
+	newNode := &Node{
+		value: value,
+		next:  s.head,
+	}
+	s.head = newNode
+}
+
+func (s *Stack) pop() int {
+	if s.head == nil {
+		return -1
+	}
+	value := s.head.value
+	s.head = s.head.next
+	return value
+}
+
+func (s *Stack) isEmpty() bool {
+	return s.head == nil
+}
+
+func (s *Stack) display() {
+	temp := s.head
+	for temp != nil {
+		fmt.Println(temp.value)
+		temp = temp.next
+	}
+	fmt.Println()
+}
+
+type Queue struct {
+	head *Node
+	tail *Node
+}
+
+func (q *Queue) enqueue(value int) {
+	newNode := &Node{value: value}
+	if q.tail != nil {
+		q.tail.next = newNode
+	}
+	q.tail = newNode
+	if q.head == nil {
+		q.head = newNode
+	}
+}
+
+func (q *Queue) dequeue() int {
+	if q.head == nil {
+		return -1
+	}
+
+	value := q.head.value
+	q.head = q.head.next
+	if q.head == nil {
+		q.tail = nil
+	}
+	return value
+}
+
+func (q *Queue) isEmpty() bool {
+	return q.head == nil
+}
+
+func (q *Queue) display() {
+	temp := q.head
+	for temp != nil {
+		fmt.Println(temp.value)
+		temp = temp.next
+	}
+	fmt.Println()
+}
+
+func convertToStack(queue Queue) *Stack {
+	stack := &Stack{}
+
+	for !queue.isEmpty() {
+		stack.push(queue.dequeue())
+	}
+	return stack
+}
+
+func main() {
+	queue := &Queue{}
+
+	queue.enqueue(10)
+	queue.enqueue(20)
+	queue.enqueue(30)
+	queue.enqueue(60)
+	queue.display()
+
+	stack := convertToStack(*queue)
+	stack.display()
+
+	stack.pop()
+	stack.display()
+}
--- main.go
+++ main.go
@@ -3,19 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{}
+	arr := []int{1, 21, 33, 4, 50, 6, 7}
+	fmt.Println(mergeSort(arr))
+	fmt.Println(quickSort(arr))
+}
 
-	for i := 7; i < 100; i++ {
-		if i%7 == 0 {
-			arr = append(arr, i)
+func mergeSort(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+
+	mid := (len(arr) / 2)
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	combined := []int{}
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			combined = append(combined, left[i])
+			i++
+		} else {
+			combined = append(combined, right[j])
+			j++
 		}
 	}
-	fmt.Println(arr)
 
-	sqr := make(map[int]int)
-	for _, val := range arr {
-		sqr[val] = val * val
+	if i < len(left) {
+		combined = append(combined, left[i:]...)
 	}
-	fmt.Println(sqr)
+	if j < len(right) {
+		combined = append(combined, right[j:]...)
+	}
+	return combined
+}
 
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	pivot := arr[len(arr)/2]
+	left, right := []int{}, []int{}
+
+	for _, v := range arr {
+		if v < pivot {
+			left = append(left, v)
+		} else if v > pivot {
+			right = append(right, v)
+		}
+	}
+
+	left = quickSort(left)
+	right = quickSort(right)
+	return append(append(left, pivot), right...)
 }
